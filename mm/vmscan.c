29ee183d02ca905c8c634f50eaf0cb8bfe91de61
Yu Zhao <yuzhao@google.com>
2021-06-30 18:49:48 -0700

    mm/vmscan.c: fix potential deadlock in reclaim_pages()
    Theoretically without the protect from memalloc_noreclaim_save() and
    memalloc_noreclaim_restore(), reclaim_pages() can go into the block
    I/O layer recursively and deadlock.
    
    Querying 'reclaim_pages' in our kernel crash databases didn't yield
    any results. So the deadlock seems unlikely to happen. A possible
    explanation is that the only user of reclaim_pages(), i.e.,
    MADV_PAGEOUT, is usually called before memory pressure builds up,
    e.g., on Android and Chrome OS. Under such a condition, allocations in
    the block I/O layer can be fulfilled without diverting to direct
    reclaim and therefore the recursion is avoided.
    
    Link: https://lkml.kernel.org/r/20210622074642.785473-1-yuzhao@google.com
    Link: https://lkml.kernel.org/r/20210614194727.2684053-1-yuzhao@google.com
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1533,6 +1533,7 @@ unsigned long reclaim_clean_pages_from_list(struct zone *zone,
 	struct reclaim_stat stat;
 	unsigned long nr_reclaimed;
 	LIST_HEAD(clean_pages);
+	unsigned int noreclaim_flag;
 
 	list_for_each_entry_safe(page, next, page_list, lru) {
 		if (page_is_file_cache(page) && !PageDirty(page) &&
@@ -1542,8 +1543,16 @@ unsigned long reclaim_clean_pages_from_list(struct zone *zone,
 		}
 	}
 
+	/*
+	 * We should be safe here since we are only dealing with file pages and
+	 * we are not kswapd and therefore cannot write dirty file pages. But
+	 * call memalloc_noreclaim_save() anyway, just in case these conditions
+	 * change in the future.
+	 */
+	noreclaim_flag = memalloc_noreclaim_save();
 	nr_reclaimed = shrink_page_list(&clean_pages, zone->zone_pgdat, &sc,
 			TTU_IGNORE_ACCESS, &stat, true);
+	memalloc_noreclaim_restore(noreclaim_flag);
 	list_splice(&clean_pages, page_list);
 	mod_node_page_state(zone->zone_pgdat, NR_ISOLATED_FILE, -nr_reclaimed);
 	/*
@@ -2204,6 +2213,7 @@ unsigned long reclaim_pages(struct list_head *page_list)
 	LIST_HEAD(node_page_list);
 	struct reclaim_stat dummy_stat;
 	struct page *page;
+	unsigned int noreclaim_flag;
 	struct scan_control sc = {
 		.gfp_mask = GFP_KERNEL,
 		.priority = DEF_PRIORITY,
@@ -2211,6 +2221,8 @@ unsigned long reclaim_pages(struct list_head *page_list)
 		.may_unmap = 1,
 		.may_swap = 1,
 	};
+
+	noreclaim_flag = memalloc_noreclaim_save();
 
 	while (!list_empty(page_list)) {
 		page = lru_to_page(page_list);
@@ -2249,6 +2261,8 @@ unsigned long reclaim_pages(struct list_head *page_list)
 			putback_lru_page(page);
 		}
 	}
+
+	memalloc_noreclaim_restore(noreclaim_flag);
 
 	return nr_reclaimed;
 }
